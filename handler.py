1e6716aaeee9eab4781940d32f20ce3ef5b0866c
vs-solution7 <vladyslav.web.solutions@gmail.com>
2022-05-20 01:51:31 -0700

    feat: Implement get customer name by id

--- handler.py
+++ handler.py
@@ -1,24 +1,65 @@
 import json
+import psycopg2
+from os import environ
+
+DB_CONN = environ.get('DB_CONN')
+
+header = {'access-control-allow-origin': '*'}
+
+conn = None
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def open_connection():
+    global conn
+    try:
+        if conn is None:
+            conn = psycopg2.connect(DB_CONN)
+        elif conn.closed:
+            conn = psycopg2.connect(DB_CONN)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    except Exception as err:
+        print("[ERROR] Unexpected error: Could not connect to DB instance.")
+        raise err
 
-    return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
+def make_response(statusCode, body, header):
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        'body': json.dumps(body),
+        'statusCode': statusCode,
+        'headers': header
     }
-    """
+
+
+def main(event, context):
+    try:
+        open_connection()
+        customer_id = event['pathParameters']['id']
+        print(f"[INFO] customer id: {customer_id}")
+
+        with conn:
+            with conn.cursor() as cur:
+                try:
+                    query = """
+                                SELECT * FROM customers WHERE id = %s
+                            """
+                    cur.execute(query, (customer_id,))
+                    record = cur.fetchone()
+                    customer_name = record[1]
+
+                except Exception as err:
+                    print(f"[ERROR] GetCustomer: ", err)
+                    body = {
+                        "message": "Could not retrieve customer name.",
+                    }
+                    return make_response(400, body, header)
+
+    finally:
+        if conn is not None and not conn.closed:
+            conn.close()
+
+    body = {
+        "message": "You have retrieved customer name successfully!",
+        "name": customer_name
+    }
+
+    return make_response(200, body, header)
